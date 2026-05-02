2c8a8295aa653c5f2063fc7e56df0c581c69ea78
lombifear-lgtm <lombifear@gmail.com>
2026-05-02 17:21:08 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,52 +40,7 @@ class MainWin(QWidget):
        self.resize(win_wtdth, win_height)
        self.move(win_x, win_y)
 
-class Testwin(QWidget):
-    def timer_test(self):
-        global time 
-        time = QTime(0, 1, 0)
-        self.timer = QTimer()
-        self.time.timeout.connect(self.timer3Event)
-        self.timer.start(1000)
-       
-       
-     def timer1Event(Self):
-        global time
-        time = time.addSecs(-1)
-        self.text_timer.setText(Time.toString("hh:mm:ss"))
-        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
-        self.text_timer.setStyleSheet("color: rgb(0,0,0")
-        if time.toString("hh:mm:ss") == "00:00:00":
-            self.timer.stop()
 
-    def timer_sits(self):
-        time = Qtime(0,0, 30)
-        self.time.timeout.connect(self.timer2Event)
-        self.timer.start(1500)
-    
-    def timer2Event(self):
-        self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
-    
-    def timer_final(self):
-        time = QTime(0,1,0)
-        self.timer.timeout.connect(self.timer3Event)
-
-    def Timer3Event(self):
-        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
-            self.text_timer.setStyleSheet("color: rgb(125,0,0)")
-        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
-            self.text_timer.setStyleSheet("color: rgb(0,100,0)")
-        else:
-            self.text_timer.setStyleSheet("color: rgb(0,0,70)")
-
-    def connects(self):
-        self.btn_test1.clicked.connect(self.timer_test)
-        self.btn_test1.clicked.connect(self.timer_test)
-        self.btn_test2.clicked.connect(self.timer_sits)
-        self.btn_test3.clicked.connect(self.timer_final)
-
-
-        
 app = QApplication([])
 mw = MainWin()
 app.exec_()
